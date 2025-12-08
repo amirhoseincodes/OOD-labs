806043e4ad7bc06313c62c50570f559a2697384d
amihoseincodes <amirhoseinworks@gmail.com>
2025-12-08 20:46:13 +0330

    fix and add correct version step1

---
+++ Step-01-without-OOD/app/src/services/EmailSender.java
@@ -0,0 +1,7 @@
+package services;
+
+class EmailSender implements MessageSender{
+    public void send(String to, String message){
+        System.out.println("Sending email to " + to + ": " + message);
+    }
+}
---
+++ Step-01-without-OOD/app/src/services/MessageSender.java
@@ -0,0 +1,5 @@
+package services;
+
+public interface MessageSender {
+    public void send(String to, String message);
+}
